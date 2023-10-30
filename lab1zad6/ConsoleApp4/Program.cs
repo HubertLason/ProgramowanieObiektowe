02784a5618e77749735017060245edc6a930670f
HubertLason <116073303+HubertLason@users.noreply.github.com>
2023-10-30 21:10:44 +0100

    Add files via upload

---
+++ lab1zad6/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            int a;
+            while (true)
+            {
+                a = InputInt();
+                if (a == 0) break;
+            }
+        }
+
+        static int InputInt()
+        {
+            int value = Convert.ToInt32(Console.ReadLine());
+            return value;
+
+        }
+
+
+
+    }
+}

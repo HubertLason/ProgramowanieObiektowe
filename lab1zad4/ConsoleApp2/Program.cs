c5c584ca95030568f534c522105b559e88e659c7
HubertLason <116073303+HubertLason@users.noreply.github.com>
2023-10-30 21:10:14 +0100

    Add files via upload

---
+++ lab1zad4/ConsoleApp2/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            double[] a = new double[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = InputDouble();
+            }
+           
+            Console.WriteLine(Suma(a));
+            Ilooczyn(a);
+            Srednia(a);
+            Maximum(a);
+            Minimum(a);
+        }
+        static double InputDouble()
+        {
+            Console.WriteLine("Podaj liczbe");
+            double value = Convert.ToDouble(Console.ReadLine());
+            return value;
+
+        }
+        static double Suma(double[] a)
+        {
+            double b = 0;
+            for (int i = 0; i < 10; i++) b=b+a[i];
+            return b;
+        }
+        static void Ilooczyn(double[] a)
+        {
+            double b = a[0];
+            for (int i = 1; i < 10; i++) b = b * a[i];
+            Console.WriteLine(b);
+        }
+        static void Srednia(double[] a)
+        {
+            Console.WriteLine(Suma(a)/10);
+        }
+        static void Maximum(double[] a)
+        {
+            Console.WriteLine(a.Max());
+        }
+        static void Minimum(double[] a)
+        {
+            Console.WriteLine(a.Min());
+        }
+
+
+
+    }
+}
---
+++ lab1zad5/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            Liczby();
+        }
+
+        static void Liczby()
+        {
+            for (int i = 20; i >= 0; i--)
+            {
+                if (i == 2 | i == 6 | i == 9 | i == 15 | i == 19) continue;
+                else Console.WriteLine(i);
+
+            }
+
+        
+        
+        }
+
+
+    }
+}
